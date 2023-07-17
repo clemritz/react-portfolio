d0c5bd8072313d47edffc8ea23cf02bd18e7429d
Clément Ritz <clement.ritz@orange.fr>
2023-07-17 18:12:26 +0200

    routes config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import Work from './Work';
+import About from './About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/work">Work</Link>
+              </li>
+              <li>
+                <Link to="/about">About me</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route path="/work" component={Work} />
+            <Route path="/about" component={About} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
