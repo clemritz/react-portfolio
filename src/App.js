ca870b6957b3a99988a5e443409d278c8a63faef
Clément Ritz <clement.ritz@orange.fr>
2023-07-19 21:59:06 +0200

    homepage project display + animation reveal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
@@ -7,8 +7,14 @@ import About from './About';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './body.css';
+import 'aos/dist/aos.css'; // Import the AOS reveal animation
+import AOS from 'aos'; // Import AOS here
 
 function App() {
+  useEffect(() => {
+    AOS.init(); // Initialize AOS inside useEffect hook
+  }, []); // Run this only once after the component is mounted
+
   return (
     <div className="App">
       <Router>
