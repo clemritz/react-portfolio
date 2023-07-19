49e43aa10952e2bd95cd1b5a1bc6d7ead989a4c1
Clément Ritz <clement.ritz@orange.fr>
2023-07-19 21:29:57 +0200

    homepage + type effect

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './home.css';
 
-export default function Home() {
+function Homepage() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="hero">
+        <div className="header">
+          <h1 className="line anim-typewriter">I am a front end developer.</h1>
+        </div>
+      </div>
+      <div className="scroll-down">
+      </div>
+    </div>
   );
 }
+
+export default Homepage;
