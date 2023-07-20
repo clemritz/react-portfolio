85eab61d7071e15067e8d6a60ddf9b0f014e07dd
Clément Ritz <clement.ritz@orange.fr>
2023-07-20 21:28:47 +0200

    work page + about me page config

--- src/About.js
+++ src/About.js
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>About me</div>
+    <div>
+      <div className='whitespace'></div>
+      <div className='whitespace'></div>
+      <div className="container">
+        <div className="hero-content">
+          <br></br>
+          <br></br>
+          <div className="row">
+            <div className="col-lg-12">
+              <h3 data-aos="fade-left" data-aos-duration="2500">about me.</h3>
+              <p data-aos="fade-up" data-aos-duration="3000">I decided to venture into web development. I recently completed the #1210 batch at Le Wagon coding bootcamp, and now I'm eager to further explore new languages and refine my skills. I am continuously learning and exploring new technologies, frameworks, and languages in the field of web development. I'm particularly interested in frontend development and JavaScript frameworks like React and Vue.
+              </p>
+              <p data-aos="fade-up" data-aos-duration="3000">I am currently working on personal projects to strengthen my web development skills.
+              I'm open to collaborating on interesting web development projects, especially those involving frontend development, user experience design, and creating interactive web applications. You can contact me on my different networks.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='whitespace'></div>
+    </div>
   );
 }
